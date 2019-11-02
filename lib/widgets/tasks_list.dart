87a544a1e5b3a7c5cedc5c20c7ee90ce6eee2b3f
Dukho <dukhochoi@gmail.com>
2019-11-02 18:35:35 +1100

    Add deleting task

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -4,27 +4,28 @@ import 'package:provider/provider.dart';
 import 'package:todo_flutter/models/task_data.dart';
 
 class TasksList extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Consumer<TaskData>(
       builder: (context, taskData, child) {
         return ListView.builder(
           itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+
             return TaskTile(
-                taskTitle: taskData.tasks[index].name,
-                isChecked: taskData.tasks[index].isDone,
-                checkboxCallback: (bool checkboxState) {
-//            setState(() {
-//              Provider.of<TaskData>(context).tasks[index].toggleDone();
-//            });
-                }
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (bool checkboxState) {
+                taskData.updateTask(task);
+              },
+              onLongPressCallback: () {
+                taskData.deleteTask(task);
+              },
             );
           },
           itemCount: taskData.taskCount,
         );
       },
-
     );
   }
 }
--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -4,8 +4,9 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
   final Function checkboxCallback;
+  final Function onLongPressCallback;
 
-  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
+  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback, this.onLongPressCallback});
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +22,7 @@ class TaskTile extends StatelessWidget {
         activeColor: Colors.lightBlueAccent,
         onChanged: checkboxCallback,
       ),
+      onLongPress:onLongPressCallback,
     );
   }
 }
